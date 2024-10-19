fde7c4e04a3c4590926f6c8b5afeb45665d3f337
pane2004 <alexlu2000clash@gmail.com>
2024-10-19 10:26:36 -0700

    add backend to handle gun events

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,40 @@
-import express from 'express';
+import express from "express";
 const app = express();
 const port = 3001;
+let liveGame = false;
 
-app.get('/api', (req, res) => {
+// Middleware to parse JSON requests
+app.use(express.json());
+
+app.get("/init", (req, res) => {
+  // Default beginner health values
+  let maxHealth = 5;
+  let maxAmmo = 3;
+  let shootCooldown = 500; // ms
+  let reloadTime = 2000; // ms
+  let userAddress = "696969696";
+  let battleContract = "sfspdfjpsdif";
+
   res.json({
-    value1: 24.25,
-    value2: 49.54,
-    value3: 1005.14
+    maxHealth: maxHealth,
+    maxAmmo: maxAmmo,
+    shootCooldown: shootCooldown,
+    reloadTime: reloadTime,
+    userAddress: userAddress,
+    battleContract: battleContract,
+    play: true,
   });
 });
 
-app.listen(port, '0.0.0.0', () => {
+app.post("/died", (req, res) => {
+  const { userAddress, battleContract } = req.body;
+  console.log("i fucking died.");
+  console.log("User Address:", userAddress);
+  console.log("Battle Contract:", battleContract);
+
+  res.sendStatus(200);
+});
+
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on http://0.0.0.0:${port}`);
 });
